1d5191cccb50282570b96403606b2dce64b50ebf
matheus cortes <matheuscbarbeiro30@gmail.com>
2024-09-03 14:19:03 -0300

    3 primeiros passos do tutorial expo

---
+++ aula1/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={{color: 'white', fontSize: '50px'}}>meu pipi no seu popo!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
